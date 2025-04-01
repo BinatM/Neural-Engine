d3d837a9c7aa224eff143a716d2422043da0de8c
ArielRichter <arielrichter1@gmail.com>
2025-04-01 19:25:41 +0300

    1.6.3V

--- top_level.sv
+++ top_level.sv
@@ -1,13 +1,25 @@
 module top_level (
     input  wire         MAX10_CLK1_50,
-    input  wire         KEY_0, // push-button, active-low
+    input  wire         KEY_0,
 
     input  wire [15:0]  FPGA_DATA_IN,
     output wire [15:0]  FPGA_DATA_OUT,
-    output wire         FPGA_OUTPUT_READY
+    output wire         FPGA_OUTPUT_READY,
+
+    // SDRAM physical pins
+    output wire [12:0]  DRAM_ADDR,
+    output wire [1:0]   DRAM_BA,
+    output wire         DRAM_CAS_N,
+    output wire         DRAM_CKE,
+    output wire         DRAM_CLK,
+    output wire         DRAM_CS_N,
+    inout  wire [15:0]  DRAM_DQ,
+    output wire         DRAM_LDQM,
+    output wire         DRAM_RAS_N,
+    output wire         DRAM_UDQM,
+    output wire         DRAM_WE_N
 );
 
-    // 1) Debounce push-button => reset
     wire db_key0;
     debounce_button #(.DELAY_MAX(100000)) key_deb (
         .clk       (MAX10_CLK1_50),
@@ -16,7 +28,6 @@ module top_level (
         .clean_out (db_key0)
     );
 
-    // 2) reset & start
     wire reset_n_sys;
     wire start_sig;
     reset_and_start rs(
@@ -26,48 +37,71 @@ module top_level (
         .start_pulse (start_sig)
     );
 
-    // 3) clock_generator
     wire clk_internal;
     clock_generator clkgen(
         .clk_in  (MAX10_CLK1_50),
         .clk_out (clk_internal)
     );
 
-    // 4) SDRAM Controller (Stub) - read only
     wire [15:0] sdram_data_out;
-    wire [15:0] sdram_data_in = 16'd0; 
-    wire        sdram_rd_en;
-    sdram_controller sdram (
-        .clk       (clk_internal),
-        .reset_n   (reset_n_sys),
-        .wr_en     (1'b0),             // not used
-        .rd_en     (sdram_rd_en),      // for LOAD
-        .data_in   (sdram_data_in),
-        .data_out  (sdram_data_out),
-        .address   (24'd0),
-        .ready     (), // unused
-        // stub signals
-        .addr      (),
-        .ba        (),
-        .cas_n     (),
-        .ras_n     (),
-        .we_n      (),
-        .clk_out   (),
-        .cke       (),
-        .cs_n      (),
-        .dq        (),
-        .ldqm      (),
-        .udqm      ()
+    wire [15:0] sdram_data_in;
+    wire [23:0] sdram_address;
+    wire        sdram_rd_en, sdram_wr_en;
+    wire        sdram_ready;
+
+    SDRAM_CONTROLLER #(
+        .G_CLK_FREQ           (50.0),
+        .G_CAS_LATENCY        (2),
+        .G_WRITE_BURST_MODE   ('0),
+        .G_BURST_LENGTH       (1),
+        .G_USE_AUTO_PRECHARGE ('0),
+        .G_BURST_TYPE         ('0),
+        .G_ADDR_WIDTH         (24),
+        .G_SDRAM_ADDR_WIDTH   (13),
+        .G_SDRAM_DATA_WIDTH   (16),
+        .G_SDRAM_COL_WIDTH    (9),
+        .G_SDRAM_ROW_WIDTH    (13),
+        .G_SDRAM_BANK_WIDTH   (2),
+        .G_T_DESL             (200_000.0),
+        .G_T_MRD              (14.0),
+        .G_T_RC               (65.0),
+        .G_T_RCD              (20.0),
+        .G_T_RP               (20.0),
+        .G_T_WR               (14.0),
+        .G_T_REFI             (7800.0)
+    ) sdram (
+        .I_CLOCK             (clk_internal),
+        .I_RESET_N           (reset_n_sys),
+        .I_ADDRESS           (sdram_address),
+        .I_DATA              (sdram_data_in),
+        .I_REQUEST           (sdram_rd_en || sdram_wr_en),
+        .I_WRITE_ENABLE      (sdram_wr_en),
+        .O_ACKNOWLEDGE       (),
+        .O_VALID             (sdram_ready),
+        .O_Q                 (sdram_data_out),
+        .O_SDRAM_A           (DRAM_ADDR),
+        .O_SDRAM_BA          (DRAM_BA),
+        .IO_SDRAM_DQ         (DRAM_DQ),
+        .O_SDRAM_CKE         (DRAM_CKE),
+        .O_SDRAM_CS          (DRAM_CS_N),
+        .O_SDRAM_RAS         (DRAM_RAS_N),
+        .O_SDRAM_CAS         (DRAM_CAS_N),
+        .O_SDRAM_WE          (DRAM_WE_N),
+        .O_SDRAM_DQML        (DRAM_LDQM),
+        .O_SDRAM_DQMH        (DRAM_UDQM),
+        .O_SDRAM_INITIALIZED ()
     );
 
-    // 5) on_chip_memory
+
+
+    // On-chip memory
     wire [15:0] mem_data_out;
     wire [15:0] mem_data_in;
     wire [10:0] mem_address;
     wire        mem_wr_en;
     wire        mem_rd_en;
 
-    on_chip_memory onchip_mem(
+    on_chip_memory onchip_mem (
         .clk              (clk_internal),
         .reset_n          (reset_n_sys),
         .wr_en            (mem_wr_en),
@@ -80,111 +114,99 @@ module top_level (
         .use_external_addr(1'b1)
     );
 
-    // 6) control_unit - load from SDRAM => on_chip_memory, then run
-    wire        ctrl_wr_en;
-    wire [10:0] ctrl_mem_address;
-    wire        start_run;
-    wire        ctrl_output_ready;
+    // Control Unit
+    wire ctrl_wr_en;
+    wire [9:0] ctrl_mem_address;
+    wire ctrl_output_ready, ctrl_start_run, ctrl_all_done;
 
-    control_unit #(.LOAD_DEPTH(256)) ctrl(
-        .clk         (clk_internal),
-        .reset_n     (reset_n_sys),
-        .start       (start_sig),
-
-        // LOAD signals
-        .sdram_rd_en (sdram_rd_en),
-        .mem_wr_en   (ctrl_wr_en),
-        .mem_address (ctrl_mem_address),
-
-        // run signals
-        .wr_en       (),  // not used
-        .rd_en       (),
-        .output_ready(ctrl_output_ready),
-        .start_run   (start_run)
+    control_unit #(.LOAD_DEPTH(69)) ctrl (
+        .clk           (clk_internal),
+        .reset_n       (reset_n_sys),
+        .start         (start_sig),
+        .sdram_rd_en   (sdram_rd_en),
+        .sdram_wr_en   (sdram_wr_en),
+        .sdram_address (sdram_address),
+        .sdram_dout    (sdram_data_out),
+        .sdram_ready   (sdram_ready),
+        .mem_wr_en     (ctrl_wr_en),
+        .mem_address   (ctrl_mem_address),
+        .output_ready  (ctrl_output_ready),
+        .start_run     (ctrl_start_run),
+        .all_done      (ctrl_all_done)
     );
 
-    // 7) generator
+    // Generator
     wire [10:0] gen_address;
     wire        gen_rd_en, gen_wr_en, gen_chip_sel;
     wire [15:0] gen_data_out;
+
     test_generator #(.ADDR_WIDTH(11)) gen (
         .clk         (clk_internal),
         .reset       (~reset_n_sys),
-        .start       (start_run),
+        .start       (ctrl_start_run),
         .address_BUS (gen_address),
-        .DATA_BUS    (gen_data_out), // generator might produce data if needed
+        .DATA_BUS    (gen_data_out),
         .rd_en       (gen_rd_en),
         .wr_en       (gen_wr_en),
+		  .output_ready(output_ready),
         .chip_sel    (gen_chip_sel)
     );
 
-    // 8) mac_core
+    // MAC core
     wire [15:0] mac_data_out;
     wire        mac_ready;
+
     mac_core dut (
         .clk          (clk_internal),
         .reset_n      (reset_n_sys),
-
-        // ** get data from on_chip_memory **
         .data_in      (mem_data_out),
-
-        // control signals from generator
         .wr_en        (gen_wr_en),
         .rd_en        (gen_rd_en),
         .chip_sel     (gen_chip_sel),
-
-        // out
         .data_out     (mac_data_out),
         .output_ready (mac_ready)
     );
 
-    // 9) validator
+    // Validator
     wire [10:0] val_address_out;
     wire        val_rd_en, val_wr_en;
     wire [15:0] val_data_to_mem;
     wire        val_done;
+
     validator #(.ADDR_WIDTH(11)) val (
         .clk          (clk_internal),
         .reset_n      (reset_n_sys),
         .output_ready (mac_ready),
         .dut_output   (mac_data_out),
-
-        // memory I/F
         .address_out  (val_address_out),
         .rd_en        (val_rd_en),
         .wr_en        (val_wr_en),
-        .mem_data_out (mem_data_out),  
+        .mem_data_out (mem_data_out),
         .data_to_mem  (val_data_to_mem),
-
         .val_done     (val_done)
     );
 
-    // 10) MUX to memory
+    // MUX
     reg [10:0] mux_address;
     reg [15:0] mux_data_in_r;
     reg        mux_wr_en, mux_rd_en_r;
 
     always @(*) begin
-        mux_wr_en   = 1'b0;
-        mux_rd_en_r = 1'b0;
-        mux_address = 11'd0;
+        mux_wr_en     = 1'b0;
+        mux_rd_en_r   = 1'b0;
+        mux_address   = 11'd0;
         mux_data_in_r = 16'd0;
 
-        // 1) control_unit load
         if (ctrl_wr_en) begin
             mux_wr_en     = 1'b1;
             mux_address   = ctrl_mem_address;
             mux_data_in_r = sdram_data_out;
-        end
-        // 2) generator read
-        else if (gen_wr_en || gen_rd_en) begin
+        end else if (gen_wr_en || gen_rd_en) begin
             mux_wr_en     = gen_wr_en;
             mux_rd_en_r   = gen_rd_en;
             mux_address   = gen_address;
-            mux_data_in_r = 16'd0; 
-        end
-        // 3) validator
-        else if (val_wr_en || val_rd_en) begin
+            mux_data_in_r = 16'd0;
+        end else if (val_wr_en || val_rd_en) begin
             mux_wr_en     = val_wr_en;
             mux_rd_en_r   = val_rd_en;
             mux_address   = val_address_out;
@@ -197,7 +219,7 @@ module top_level (
     assign mem_address = mux_address;
     assign mem_data_in = mux_data_in_r;
 
-    // 11) OUTPUT signals
+    // Output
     wire any_ready = mac_ready || val_done || ctrl_output_ready;
     assign FPGA_OUTPUT_READY = any_ready;
 
