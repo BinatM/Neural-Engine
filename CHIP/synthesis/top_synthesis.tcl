cadb3540ac3f41d1139a368e805229cdef933649
BinatM <makhlin.binat@gmail.com>
2025-04-28 11:18:32 +0300

    synthesis with FF's insted of SRAM

--- CHIP/synthesis/top_synthesis.tcl
+++ CHIP/synthesis/top_synthesis.tcl
@@ -1,5 +1,6 @@
+#RUN FROM CHIP FOLDER
 lappend search_path scripts design_data 
-lappend search_path CHIP/sram
+#lappend search_path CHIP/sram
 
 set_host_options -max_cores 8
 set TECH_FILE     "/data/tsmc/28HPCPMMWAVE/synopsys/tsmcn28_9lm6X1Z1URDL.tf"
@@ -7,26 +8,27 @@ set TECH_FILE     "/data/tsmc/28HPCPMMWAVE/synopsys/tsmcn28_9lm6X1Z1URDL.tf"
 #######################################################################
 ## Physical Library Settings
 #######################################################################
+#/project/tsmc28mmwave/users/binatmakhlin/ws/neuron/CHIP/sram/ts6n28hpcphvta64x8m4fwbso_200b_new.ndm
 create_lib -technology $TECH_FILE -ref_libs {
     /data/tsmc/28HPCPMMWAVE/synopsys/libs/tcbn28hpcplusbwp30p140.ndm
     /data/tsmc/28HPCPMMWAVE/synopsys/libs/tcbn28hpcplusbwp30p140hvt.ndm
     /data/tsmc/28HPCPMMWAVE/synopsys/libs/tcbn28hpcplusbwp30p140lvt.ndm
-    /project/tsmc28mmwave/users/binatmakhlin/ws/neuron/CHIP/sram/ts6n28hpcphvta64x8m4fwbso_200b_new.ndm
-} neuron_top.dlib
-open_lib neuron_top.dlib
+} neuron_top_no_sram.dlib
+open_lib neuron_top_no_sram.dlib
 report_ref_libs
-read_lef CHIP/sram/ts6n28hpcphvta64x8m4fwbso_200b/LEF/ts6n28hpcphvta64x8m4fwbso_200b.lef
+#read_lef CHIP/sram/ts6n28hpcphvta64x8m4fwbso_200b/LEF/ts6n28hpcphvta64x8m4fwbso_200b.lef
 
 read_parasitic_tech -tlup /data/tsmc/28HPCPMMWAVE/dig_libs/snpsflow/rcbest/crn28hpc+_1p09m+ut-alrdl_6x1z1u_rcbest.tluplus -name rcbest
 read_parasitic_tech -tlup /data/tsmc/28HPCPMMWAVE/dig_libs/snpsflow/rcworst/crn28hpc+_1p09m+ut-alrdl_6x1z1u_rcworst.tluplus -name rcworst
 
 save_lib
 analyze -format sverilog {
-    CHIP/rtl/top.sv
-    CHIP/rtl/mac.sv
-    CHIP/rtl/Control_unit.sv
-    CHIP/rtl/activation_function.sv
-    CHIP/rtl/neuron_io.sv
+    rtl/top.sv
+    rtl/mac.sv
+    rtl/Control_unit.sv
+    rtl/activation_function.sv
+    rtl/neuron_io.sv
+    rtl/input_memory.sv
 }
 elaborate top
 set_top_module top
@@ -38,7 +40,7 @@ save_block -as top_elaborate
 remove_corners   -all
 remove_modes     -all
 remove_scenarios -all
-?
+
 # Create Corners
 create_corner Fast
 create_corner Slow
@@ -57,25 +59,25 @@ create_scenario -mode FUNC -corner Slow    -name FUNC_Slow
 
 #sourse ConFiles/riscv.con
 current_scenario FUNC_Fast 
-source  CHIP/synthesis/top.sdc
+source  synthesis/top.sdc
 current_scenario FUNC_Slow 
-source  CHIP/synthesis/top.sdc
+source  synthesis/top.sdc
 
 set_auto_floorplan_constraints -core_utilization 0.7 -side_ratio {1 1} -core_offset 2
 set_lib_cell_purpose [get_lib_cells */CKL*] -include none
-compile_fusion -to top_logic_opto
+compile_fusion -to logic_opto
 #create_placement
 #legalize_placement
-?
+
 ##Power
-compile_fusion -to top_final_opto
+compile_fusion -to final_opto
 
 ## Reports Generation
-report_area > CHIP/reports/area_report.log
-report_cells > CHIP/reports/cell_count.log
+report_area > reports/area_report.log
+report_cells > reports/cell_count.log
 #report_lib_cells -objects [get_lib_cells tcbn28hpcplusbwp30p140] > CHIP/reports/lib_cells.log
-report_power > CHIP/reports/power_report.log
-report_timing > CHIP/reports/timing_report.log
-report_utilization > CHIP/reports/utilization.log
-report_qor > CHIP/reports/qor_report.log
+report_power > reports/power_report.log
+report_timing > reports/timing_report.log
+report_utilization > reports/utilization.log
+report_qor > reports/qor_report.log
 save_block -as top_final_opto
