ec462fa64a3091291275e761f7c7926aeb0c8e07
yuvaldesalto <yuval.desalto@gmail.com>
2025-06-11 19:01:46 +0300

    Update validator.sv

--- test_environment/validator.sv
+++ test_environment/validator.sv
@@ -11,12 +11,11 @@ module validator (
 );
 
 // State encoding
-typedef enum logic [2:0] {
-    VAL_IDLE            = 3'd0,
-    VAL_READ_SINGLE_BIT = 3'd1,
-    VAL_WRITE_RESULT    = 3'd2,
-    VAL_DONE            = 3'd3,
-	 CHIP_SEL            = 3'd4
+typedef enum logic [1:0] {
+    VAL_IDLE            = 2'd0,
+    VAL_READ_SINGLE_BIT = 2'd1,
+    VAL_WRITE_RESULT    = 2'd2,
+    VAL_DONE            = 2'd3
 } val_state_t;
 
 val_state_t state, next_state;
